4a7e5a61dd69568d3ad4cfb120777f3f1c7d87fb
benruwel <benruwel21@gmail.com>
2020-06-13 20:50:54 +0300

    created a general layout for the view component

---
+++ src/app/models/quote.ts
@@ -0,0 +1,21 @@
+export class Quote {
+
+    showDescription : boolean;
+    upVote : number;
+    downVote : number;
+  
+    constructor (
+      public id: number,
+      public quote: string,
+      public author: string,
+      public user : string,
+      public timePassed : Date){
+  
+        this.showDescription = false;
+        this.upVote = 0;
+        this.downVote = 0;
+  
+    }
+  
+}
+  
